41e48bdfa88a4bcd5ad8e3bba0dcca05d185611b
Ernie Fabian <erniefabian3rd@gmail.com>
2023-06-08 10:26:32 -0500

    Finished models setup

--- crackerjacksapi/models/team.py
+++ crackerjacksapi/models/team.py
@@ -4,4 +4,4 @@ class Team(models.Model):
     name = models.CharField(max_length=100)
     bio = models.CharField(max_length=255)
     image_url = models.TextField(null=True, blank=True)
-    park = models.OneToOneField("Park", on_delete=models.CASCADE)
+    park = models.OneToOneField("Park", on_delete=models.CASCADE, related_name="home_team")
