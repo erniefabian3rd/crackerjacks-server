457d5e7862faf5a7fecb6dd71ffb0e6570c5456e
Ernie Fabian <erniefabian3rd@gmail.com>
2023-06-15 15:26:56 -0500

    comment view for comments on each post

--- crackerjacksapi/views/__init__.py
+++ crackerjacksapi/views/__init__.py
@@ -3,4 +3,5 @@ from .team import TeamView
 from .park import ParkView
 from .trip import TripView
 from .post import PostView
+from .comment import CommentView
 from .crackerjacks_user import CrackerjacksUserView
